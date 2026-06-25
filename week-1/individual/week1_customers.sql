1c2b94de9d11fbd692189042c79c82db949d0aee
dmitri-postolaki <dmitripostolaki@gmail.com>
2026-06-25 16:42:06 +0300

    week1 customers /week1 SQL exploration notes for customers
    Added SQL exploration notes for week 1, detailing customer data analysis and findings.

---
+++ week-1/individual/week1_customers.sql
@@ -0,0 +1,21 @@
+Nädal 1: SQL Basics -- UrbanStyle'i andmete uurimine
+
+## Mida ma tegin
+- Uurisin customers tabelit SQL päringutega
+- Leidsin 3150 klienti.
+  Puuduvad eesnimed - 0
+  Puuduvad emailid 380
+  Kokku_emaile 3150
+  unikaalseid_emaile 2640
+
+  - Osalesin meeskonna andmemaastiku koostamisel
+
+## Peamised õpid
+- uued SQL päringud
+
+## Failid
+- `week1_[tabel]_exploration.sql` -- minu SQL päringud
+- `week1_results_screenshot.png` -- tulemuste pilt
+
+## Meeskonna töö
+- [Link meeskonna Data Landscape slaidile]
