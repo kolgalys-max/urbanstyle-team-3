e11229d6b0845ff8413132cf17f27fd7626cfa05
germomatt <germo.matt@gmail.com>
2026-07-08 15:05:20 +0300

    Add files via upload

---
+++ week-3/week3_roll_b_kadunud_kliendid Raport.sql
@@ -0,0 +1,10 @@
+--Kadunud klientide raport (SLAIDILE INFO)
+
+--Ostuta kliendid: 599 registreerunud kasutajat ei ole sooritanud veel ühtegi ostu.
+
+--Kriitilised piirkonnad: Üle poole passiivsetest klientidest asub Tallinnas (231) ja Tartus (138).
+
+--Soovitus tagasivõitmiseks: Käivitada sihitud e-maili kampaania (fookusega suurlinnadele), mis pakub ostule suunamiseks piiratud kehtivusajaga esmaostusoodustust.
+
+--Lisaanalüüsi äriline järeldus
+--Talvine registreerimiste hüpe (nov 2024 – veebr 2025) tõi lehele peamiselt kampaaniaturiste; see näitab, et kuigi uute kontaktide hankimine (Lead Gen) on edukas, on registreerumisjärgne konversioon esmaostuks ebaefektiivne ja vajab optimeerimist.
